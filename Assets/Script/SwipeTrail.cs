681718de300d070358690029e7262c8755f637df
Kevin <kevin18.liem@gmail.com>
2018-03-24 22:53:09 +0700

    Drawing Board Functionality

---
+++ Assets/Script/SwipeTrail.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class SwipeTrail : MonoBehaviour {
+	public GameObject trailPrefab;
+	GameObject thisTrail;
+	Vector3 startPos;
+	Plane objPlane;
+
+	private void Start() {
+		objPlane = new Plane(Camera.main.transform.forward*-1,this.transform.position);
+	}
+	private void Update() {
+		if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)|| Input.GetMouseButtonDown(0))) {
+			// thisTrail = (GameObject) Instantiate(trailPrefab,Input.mousePosition,Quaternion.identity);
+			// Debug.Log(thisTrail.transform.position);
+			Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float rayDistance;
+			if(objPlane.Raycast(mRay, out rayDistance))
+				startPos = mRay.GetPoint(rayDistance);
+			thisTrail = (GameObject) Instantiate(trailPrefab,startPos,Quaternion.identity);
+			Debug.Log(thisTrail.transform.position);
+			//Debug.Log(startPos.x + " " + startPos.y + " " + startPos.z);
+		} else if(((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)|| Input.GetMouseButton(0))) {
+			Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float rayDistance;
+			if(objPlane.Raycast(mRay,out rayDistance))
+				thisTrail.transform.position = mRay.GetPoint(rayDistance);
+			// Debug.Log("masuk jalan");
+		// } else if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || Input.GetMouseButtonUp(0)) {
+			// if(Vector3.Distance(thisTrail.transform.position, startPos) < 0.01)
+			// 	Destroy(thisTrail);
+			// Debug.Log("masuk end");
+		}
+	}
+}
