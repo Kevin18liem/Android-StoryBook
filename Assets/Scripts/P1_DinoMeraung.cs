37896701c971c1fcb9c28184748759faf14021c9
Edwin Tandiono <tandiono98@gmail.com>
2018-03-24 16:55:50 +0700

    Added Page 1 animations

---
+++ Assets/Scripts/P1_DinoMeraung.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1_DinoMeraung : MonoBehaviour {
+
+	private Animator anim;		// dino's animator
+
+	// Use this for initialization
+	void Start () {
+
+		// initialize animator
+		anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+		{
+			Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			RaycastHit raycastHit;
+			if (Physics.Raycast(raycast, out raycastHit))
+			{
+				Debug.Log("Tap detected");
+				if (raycastHit.collider.name == gameObject.name)
+				{
+					Debug.Log(gameObject.name + " clicked");
+					anim.SetTrigger("meraung");
+				}
+					
+			}
+		}
+		
+	}
+}
