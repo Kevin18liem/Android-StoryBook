dc7e127df0e30cdd7a646aeba34dea2dc78c97cc
clement.andreas97 <clement.andreas97@gmail.com>
2018-03-24 22:02:03 +0700

    Added TextItem Class, Added Text Part GUI

---
+++ Assets/Scripts/TextItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TextItemClass
+{
+	public class TextItem {
+		string text;
+		Color color;
+
+		public TextItem (){
+			text = "";
+			color = Color.white;
+		}
+
+		public TextItem (string text, Color color) {
+			this.text = text;
+			this.color = color;
+		}
+
+		public string getText() {
+			return text;
+		}
+
+		public Color getColor() {
+			return color;
+		}
+	}
+}
+
---
+++ Assets/Scripts/guiText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TextItemClass;
+
+public class guiText : MonoBehaviour {
+
+	TextItem[] stringList;
+	Rect startRect;
+	float totalWidth;
+
+	// Use this for initialization
+	void Start () {
+		startRect = new Rect (0,0,100,20);
+		stringList = new TextItem[4];
+		stringList [0] = new TextItem ("Test ", Color.white);
+		stringList [1] = new TextItem ("Test1 ", Color.white);
+		stringList [2] = new TextItem ("Test2 ", Color.white);
+		stringList [3] = new TextItem ("Test3 ", Color.white);
+		totalWidth = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI() {
+		Rect theRect = startRect;
+		//		GUIStyle theStyle = GUI.skin.box;
+		GUIStyle theStyle = new GUIStyle();
+		theStyle.fontSize = 12;
+		theStyle.normal.textColor = Color.red;
+
+		Color oldColor = GUI.color;
+		foreach (TextItem thisItem in stringList) {
+			//			Debug.Log (thisItem.getText ());
+			GUIContent theContent = new GUIContent (thisItem.getText());
+			Vector2 theSize = theStyle.CalcSize (theContent);
+			GUI.color = thisItem.getColor();
+			theRect.width = theSize.x;
+			GUI.Box (theRect, theContent, theStyle);
+			GUI.color = oldColor;
+			theRect.x += theSize.x;
+		}
+		GUI.color = oldColor;
+	}
+}
+
