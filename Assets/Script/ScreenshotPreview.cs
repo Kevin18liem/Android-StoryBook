425594ff1c0a445d04fe32a0db76a0c77d8533a7
Kevin <kevin18.liem@gmail.com>
2018-03-30 20:48:12 +0700

    Add button and changePage script

---
+++ Assets/Script/ScreenshotPreview.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+}
---
+++ Assets/Script/changePage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changePage : MonoBehaviour {
+	[SerializeField]
+	public string sceneName;
+	public void changeMenuScene() {
+		SceneManager.LoadScene (sceneName);	
+	}
+}
