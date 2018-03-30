05c0b4c6944cb3f84ed8164fbcb8998e943bf845
Kevin <kevin18.liem@gmail.com>
2018-03-30 17:05:05 +0700

    Add Screenshot functionality

---
+++ Assets/Script/HiResScreenShots.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HiResScreenShots : MonoBehaviour {
+	public int resWidth = 4300; 
+	public int resHeight = 3300;
+	public KeyCode takeScreenshotKey = KeyCode.S;
+	public int screenshotCount = 0;
+
+	private void Start() {
+	}
+	private void Update() {
+		if (Input.GetKeyDown(takeScreenshotKey))
+	    {
+	        StartCoroutine(captureScreenshot());
+	    }
+	}
+	IEnumerator captureScreenshot()
+	{
+	    yield return new WaitForEndOfFrame();
+		string path = "Screenshots/"+"screenshot1.png";
+
+		Texture2D screenImage = new Texture2D(Screen.width-500, Screen.height-500);
+	    //Get Image from screen
+	    screenImage.ReadPixels(new Rect(500, 500, Screen.width-500, Screen.height-500), 0, 0);
+	    screenImage.Apply();
+	    //Convert to png
+	    byte[] imageBytes = screenImage.EncodeToPNG();
+
+	    //Save image to file
+	    System.IO.File.WriteAllBytes(path, imageBytes);
+	}
+}
