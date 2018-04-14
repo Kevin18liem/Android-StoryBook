48b24e5792ae78fc18322c3d5c0d8a276f426ba6
Kevin <kevin18.liem@gmail.com>
2018-04-14 16:02:04 +0700

    Insert Page 6 asset

--- Assets/Script/TiltOpacity.cs
+++ Assets/Script/TiltOpacity.cs
@@ -5,37 +5,58 @@ using UnityEngine;
 public class TiltOpacity : MonoBehaviour {
 	private float opacityValueFirstFamily;
 	private float opacityValueAnotherFamily;
-	private float inputGyroValue;
 	public GameObject anotherFamily;
-	public KeyCode takeOpacityChange = KeyCode.S;
+	public KeyCode takeOpacityChangeFirstFamily = KeyCode.F;
+	public KeyCode takeOpacityChangeAnotherFamily = KeyCode.J;
+
 	public float changeValue;
 	void Start () {
 		opacityValueFirstFamily = gameObject.GetComponent<SpriteRenderer>().color.r;
 		opacityValueAnotherFamily = anotherFamily.GetComponent<SpriteRenderer> ().color.r;
 		Input.gyro.enabled = true;
-		inputGyroValue = Input.gyro.rotationRate.y;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.gyro.attitude.x < -0.1f) {
+		if (Input.gyro.attitude.x < -0.1f ) {
 			//Debug.Log (opacityValue);
 			Debug.Log (Input.gyro.attitude.x);
-			if (opacityValueAnotherFamily >= 0.2f) {
-				opacityValueFirstFamily += changeValue;
-				opacityValueAnotherFamily -= changeValue;
-				gameObject.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueFirstFamily);
-				anotherFamily.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueAnotherFamily);
-			}
+			ChangeFirstFamilyOpacity ();
+
 		} 
 		if (Input.gyro.attitude.x > 0.18f) {
-			if (opacityValueFirstFamily >= 0.2f) {
-				opacityValueFirstFamily -= changeValue;
-				opacityValueAnotherFamily += changeValue;
-				gameObject.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueFirstFamily);
-				anotherFamily.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueAnotherFamily);
+			ChangeAnotherFamilyOpacity();
+		}
+
+		if(Input.GetKeyDown(takeOpacityChangeFirstFamily)) {
+			ChangeFirstFamilyOpacity ();
+		}
+		if(Input.GetKeyDown(takeOpacityChangeAnotherFamily)) {
+			ChangeAnotherFamilyOpacity();
+		}
+	}
+
+	void ChangeFirstFamilyOpacity() {
+		if (opacityValueAnotherFamily >= 0.2f) {
+			opacityValueAnotherFamily -= changeValue;
+			if (opacityValueFirstFamily < 1f) {
+				opacityValueFirstFamily += changeValue;
 			}
+
+			gameObject.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueFirstFamily);
+			anotherFamily.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueAnotherFamily);
+		}
+	}
+
+	void ChangeAnotherFamilyOpacity() {
+		if (opacityValueFirstFamily >= 0.2f) {
+			opacityValueFirstFamily -= changeValue;
+			if (opacityValueAnotherFamily < 1f) {
+				opacityValueAnotherFamily += changeValue;
+			}
+			gameObject.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueFirstFamily);
+			anotherFamily.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, opacityValueAnotherFamily);
 		}
 	}
 }
