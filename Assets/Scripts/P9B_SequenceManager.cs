d18d2c5b98128eb1c2f4b0153696a0999356f9bc
Edwin Tandiono <tandiono98@gmail.com>
2018-04-15 18:56:58 +0700

    Done page 9b

---
+++ Assets/Scripts/P9B_SequenceManager.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P9B_SequenceManager : MonoBehaviour {
+
+	public GameObject bgAyah;
+	public GameObject spriteAyah;
+	public GameObject balonAyahKecil;
+	public GameObject balonAyahBesar;
+	public GameObject subtitleAyahKecil;
+	public GameObject subtitleAyahBesar;
+	public GameObject spriteAnak;
+	public GameObject balonAnak;
+	public GameObject subtitleAnak;
+	public GameObject nextPageButton;
+	public bool inSequence;
+
+	private int sequence;
+	private bool inCoroutine;
+
+	// Use this for initialization
+	void Start () {
+
+		sequence = 0;
+		inSequence = false;
+		inCoroutine = false;
+
+		Debug.Log ("disabling ayah");
+		bgAyah.SetActive (false);
+		spriteAyah.SetActive (false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!inSequence && !inCoroutine) {
+			inSequence = true;
+			switch (sequence) {
+			case 0:
+				{
+					Debug.Log ("seq 0 : delaying");
+					StartCoroutine (Delay (1));
+					sequence++;
+					inSequence = false;
+					break;
+				}
+			case 1:
+				{
+					Debug.Log ("seq 1 : enabling ayah");
+					bgAyah.SetActive (true);
+					balonAyahKecil.SetActive (true);
+					spriteAyah.SetActive (true);
+					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAyahKecil.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAyahKecil.GetComponent<P9B_Subtitles> ().DoSub (0);
+					sequence++;
+					break;
+				}
+			case 2:
+				{
+					Debug.Log ("seq 2 : anak reply");
+					balonAyahKecil.SetActive (false);
+					balonAnak.SetActive (true);
+					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (0);
+					sequence++;
+					break;
+				}
+			case 3: 
+				{
+					Debug.Log ("seq 3 : anak reply 2");
+					spriteAnak.GetComponent<Animator> ().SetFloat ("mood", 0);
+					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (1);
+					sequence++;
+					break;
+				}
+			case 4:
+				{
+					Debug.Log ("seq 4 : ayah reply");
+					balonAnak.SetActive (false);
+					balonAyahKecil.SetActive (true);
+					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAyahKecil.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAyahKecil.GetComponent<P9B_Subtitles> ().DoSub (1);
+					sequence++;
+					break;
+				}
+			case 5:
+				{
+					Debug.Log ("seq 5 : ayah reply 2");
+					balonAyahKecil.SetActive (false);
+					balonAyahBesar.SetActive (true);
+					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAyahBesar.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAyahBesar.GetComponent<P9B_Subtitles> ().DoSub (0);
+					sequence++;
+					break;
+				}
+			case 6:
+				{
+					Debug.Log ("seq 6 : ayah reply 3");
+					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAyahBesar.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAyahBesar.GetComponent<P9B_Subtitles> ().DoSub (1);
+					sequence++;
+					break;
+				}
+			case 7:
+				{
+					Debug.Log ("seq 7 : anak reply 3");
+					balonAyahBesar.SetActive (false);
+					balonAnak.SetActive (true);
+					spriteAnak.GetComponent<Animator> ().SetFloat ("mood", 1);
+					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (2);
+					sequence++;
+					break;
+				}
+			case 8:
+				{
+					Debug.Log ("seq 8 : ayah reply 4");
+					balonAnak.SetActive (false);
+					balonAyahKecil.SetActive (true);
+					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAyahKecil.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAyahKecil.GetComponent<P9B_Subtitles> ().DoSub (2);
+					sequence++;
+					break;
+				}
+			case 9:
+				{
+					Debug.Log ("seq 9 : anak reply 4");
+					balonAyahKecil.SetActive (false);
+					balonAnak.SetActive (true);
+					spriteAnak.GetComponent<Animator> ().SetFloat ("mood", 1);
+					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (3);
+					sequence++;
+					break;
+				}
+			case 10:
+				{
+					balonAnak.SetActive (false);
+					nextPageButton.SetActive (true);
+					break;
+				}
+			default:
+				{
+					break;
+				}
+			}
+		}
+
+	}
+
+	IEnumerator Delay(float sec) {
+		inCoroutine = true;
+		yield return new WaitForSeconds (sec);
+		inCoroutine = false;
+	}
+
+}
---
+++ Assets/Scripts/P9B_Subtitles.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TextItemClass;
+
+public class P9B_Subtitles : MonoBehaviour {
+
+	public TextItem[] texts;		// texts to be displayed
+	public float fade_speed = 1;	// fade speed
+	public char newline_char = '$';	// char to be detected as newline
+	public GameObject bubble;
+	public GameObject sprite;
+
+	private string text_buffer;		// buffer to save string
+	private int wordset;			// which wordset to display
+	private int idx;				// which word to style
+	private bool waiting;			// true if waiting
+	private bool in_anim;			// in fade animation
+	private string highlighted;		// highlighted part
+	private CanvasGroup cg;			// canvas group with alpha
+	private IEnumerator speller;
+	private GameObject seqManager;
+	private bool subAllowed = false;
+
+	// Use this for initialization
+	void Start () {
+
+		// initialization
+		cg = gameObject.GetComponent<CanvasGroup> ();
+
+		idx = 0;
+		wordset = 0;
+		waiting = false;
+		in_anim = false;
+		cg.alpha = 0;
+		cg.interactable = false;
+		seqManager = GameObject.Find ("SequenceManager");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (subAllowed) {
+			// play text if not in fade animation and waiting for input and not end of texts
+			if (!waiting && !in_anim && wordset < texts.Length) {
+				if (idx < texts [wordset].words.Length) {
+					speller = Spell (texts [wordset].words [idx].delay);
+					StartCoroutine (speller);
+				}
+			}
+		}
+
+	}
+
+	public void DoSub(int _wordset) {
+		idx = 0;
+		wordset = _wordset;
+		subAllowed = true;
+	}
+
+	IEnumerator Spell (float sec) {
+
+		// if beginning new text, go init
+		if (idx == 0) {
+			InitText ();
+		}
+
+		// make sure animation finished playing
+		if (!in_anim) {
+			waiting = true;
+			yield return new WaitForSeconds (sec);
+
+			// Highlight
+			HighlightText();
+
+			// increment
+			idx++;
+			waiting = false;
+
+		}
+
+		// if end of text ask for input
+		if (idx == texts[wordset].words.Length) {
+			subAllowed = false;
+			bubble.GetComponent<P9B_Bubble> ().allowFadeOut = true;
+			sprite.GetComponent<Animator> ().SetTrigger ("idle");
+		}
+	}
+
+	public void FadeOut() {
+		StartCoroutine (Fade (false));
+	}
+
+	IEnumerator Fade(bool fadeIn) {
+		in_anim = true;
+		if (fadeIn) {
+			while (cg.alpha < 1) {
+				cg.alpha += Time.deltaTime / 2 * fade_speed;
+				yield return null;
+			}
+			cg.interactable = true;
+		} else {
+			cg.interactable = false;
+			while (cg.alpha > 0) {
+				cg.alpha -= Time.deltaTime / 2 * fade_speed;
+				yield return null;
+			}
+			Debug.Log ("seq set to false");
+			seqManager.GetComponent<P9B_SequenceManager> ().inSequence = false;
+		}
+		in_anim = false;
+	}
+
+	void InitText () {
+		text_buffer = "";
+		for (int i = 0; i < texts[wordset].words.Length; i++) {
+			if (i == 0)
+				text_buffer = texts[wordset].words [i].text;
+			else
+				text_buffer = text_buffer + " " + texts[wordset].words [i].text;
+		}
+
+		text_buffer = text_buffer.Replace (newline_char, '\n');
+
+		GetComponent<Text> ().text = text_buffer;
+		StartCoroutine (Fade (true));
+	}
+
+	void ChangeText() {
+		if (wordset != texts.Length - 1) {
+			StartCoroutine (Fade (false));
+			idx = 0;
+			wordset++;
+		} else {
+			idx = texts[wordset].words.Length;
+			HighlightText ();
+		}
+	}
+
+	void HighlightText() {
+		text_buffer = "";
+		for (int i = 0; i < texts [wordset].words.Length; i++) {
+			if (idx == i) {
+				highlighted = "<color=\"#e67300\">" + texts [wordset].words [i].text + "</color>";
+				if (i == 0)
+					text_buffer = "<b>" + highlighted + "</b>";
+				else
+					text_buffer = text_buffer + " <b>" + highlighted + "</b>";
+			} else {
+				if (i == 0)
+					text_buffer = texts [wordset].words [i].text;
+				else
+					text_buffer = text_buffer + " " + texts [wordset].words [i].text;
+			}
+		}
+
+		text_buffer = text_buffer.Replace (newline_char, '\n');
+
+		GetComponent<Text> ().text = text_buffer;
+	}
+
+}
