5d7627d26ce50207d420a024b2d170d7a0d8f6b6
Edwin Tandiono <tandiono98@gmail.com>
2018-04-22 14:48:29 +0700

    Added sequences to page 8

---
+++ Assets/Scripts/P8_SequenceManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P8_SequenceManager : MonoBehaviour {
+
+	public GameObject subHolder;
+	public GameObject subtitle;
+	public GameObject anakSprite;
+	public GameObject ibuSprite;
+	public GameObject telepon;
+	public GameObject videocall;
+	public bool inSequence;
+
+	public int sequence;
+	private bool inCoroutine;
+
+	// Use this for initialization
+	void Start () {
+
+		sequence = 0;
+		inSequence = false;
+		inCoroutine = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!inSequence && !inCoroutine) {
+			inSequence = true;
+			switch (sequence) {
+			case 0: // anak ngomong, play subtitle 1
+				{
+					subtitle.GetComponent<P8_Subtitle> ().DoSub (0);
+					break;
+				}
+			case 1: // ibu gerak, muncul tombol
+				{
+					ibuSprite.GetComponent<Animator> ().SetTrigger ("gerak");
+					telepon.GetComponent<Animator> ().SetTrigger ("muncul");
+					telepon.GetComponent<P8_ClickableBaloon> ().allowClick = true;
+					videocall.GetComponent<Animator> ().SetTrigger ("muncul");
+					videocall.GetComponent<P8_ClickableBaloon> ().allowClick = true;
+					break;
+				}
+			default:
+				{
+					break;
+				}
+			}
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/P8_SubtitleHolder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P8_SubtitleHolder : MonoBehaviour {
+
+	public bool allowClick = false;	// true if trigger allowed
+	public float speed = 1;				// animation speed
+	public GameObject subtitle;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.touchCount > 0) && (Input.GetTouch (0).phase == TouchPhase.Began)) {
+
+			if (allowClick) {
+				Ray raycast = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+				RaycastHit raycastHit;
+				if (Physics.Raycast (raycast, out raycastHit)) {
+					if (raycastHit.collider.name == gameObject.name) {
+						//subtitle.GetComponent<P8_Subtitle> ().FadeOut ();
+						allowClick = false;
+					}
+
+				}
+			} else {
+				subtitle.GetComponent<P8_Subtitle> ().setToEnd ();
+			}
+		} else if (Input.GetMouseButtonDown (0)) {
+			if (allowClick) {
+				Ray raycast = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit raycastHit;
+				if (Physics.Raycast (raycast, out raycastHit)) {
+					if (raycastHit.collider.name == gameObject.name) {
+						//subtitle.GetComponent<P8_Subtitle> ().FadeOut ();
+						allowClick = false;
+					}
+
+				}
+			} else {
+				subtitle.GetComponent<P8_Subtitle> ().setToEnd ();
+			}
+		}
+
+	}
+}
