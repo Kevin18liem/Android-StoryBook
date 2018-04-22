4377b03dde64f639fcb0940d10434e817faf3a75
Edwin Tandiono <tandiono98@gmail.com>
2018-04-22 13:24:32 +0700

    Finished page 15

---
+++ Assets/Scripts/P15_Mobil.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P15_Mobil : MonoBehaviour {
+
+	public bool allowClick = false;
+	public GameObject button;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.touchCount > 0) && (Input.GetTouch (0).phase == TouchPhase.Began) && allowClick) {
+			Ray raycast = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+			RaycastHit raycastHit;
+			if (Physics.Raycast (raycast, out raycastHit)) {
+				if (raycastHit.collider.name == gameObject.name) {
+					anim.SetTrigger ("jalan");
+				}
+
+			}
+		} else if (Input.GetMouseButtonDown (0) && allowClick) {
+			Ray raycast = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit raycastHit;
+			if (Physics.Raycast (raycast, out raycastHit)) {
+				if (raycastHit.collider.name == gameObject.name) {
+					anim.SetTrigger ("jalan");
+				}
+
+			}
+		}
+			
+	}
+
+	public void FinishPage() {
+		button.SetActive (true);
+	}
+
+}
---
+++ Assets/Scripts/P15_SequenceManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P15_SequenceManager : MonoBehaviour {
+
+	public GameObject subHolder;
+	public GameObject subtitle;
+	public GameObject keluargaSprite;
+	public GameObject mobilSprite;
+	public bool inSequence;
+
+	public int sequence;
+	private bool inCoroutine;
+
+	// Use this for initialization
+	void Start () {
+
+		sequence = 0;
+		inSequence = false;
+		inCoroutine = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (!inSequence && !inCoroutine) {
+			inSequence = true;
+			switch (sequence) {
+			case 0: // idle, play subtitle 1
+				{
+					subtitle.GetComponent<P15_Subtitle> ().DoSub (0);
+					break;
+				}
+			case 1: // wait for pinch
+				{	
+					Debug.Log ("waiting for pinch");
+					keluargaSprite.GetComponent<P15_Keluarga> ().allowPinch = true;
+
+					break;
+				}
+			case 2: // ayah masuk mobil
+				{
+					Debug.Log ("ayah masuk mobil");
+					mobilSprite.GetComponent<Animator> ().SetTrigger ("dadah");
+					subtitle.GetComponent<P15_Subtitle> ().DoSub (1);
+					break;
+				}
+			case 3: // mobil jalan
+				{
+					mobilSprite.GetComponent<P15_Mobil> ().allowClick = true;
+					break;
+				}
+			default:
+				{
+					break;
+				}
+			}
+		}
+
+	}
+
+	public void FinishPinch() {
+		inSequence = false;
+		sequence++;
+	}
+
+}
