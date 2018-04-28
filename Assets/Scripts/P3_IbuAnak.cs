b53dd052b58d0b24709ae6a438331b1743d93aea
Edwin Tandiono <tandiono98@gmail.com>
2018-04-28 15:12:21 +0800

    Done page 3

---
+++ Assets/Scripts/P3_IbuAnak.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P3_IbuAnak : MonoBehaviour {
+
+	public bool allowTap = false;
+
+	private GameObject seqManager;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		seqManager = GameObject.Find ("SequenceManager");
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.touchCount > 0) && (Input.GetTouch (0).phase == TouchPhase.Began) && allowTap) {
+
+			Ray raycast = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+			RaycastHit raycastHit;
+			if (Physics.Raycast (raycast, out raycastHit)) {
+				if (raycastHit.collider.name == gameObject.name) {
+					anim.SetTrigger ("gerak");
+				}
+			}
+				
+		} else if (Input.GetMouseButtonDown (0) && allowTap) {
+			Ray raycast = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit raycastHit;
+			if (Physics.Raycast (raycast, out raycastHit)) {
+				if (raycastHit.collider.name == gameObject.name) {
+					anim.SetTrigger ("gerak");
+
+				}
+			}
+		}
+
+	}
+
+	public void NotifyAnimDone() {
+		seqManager.GetComponent<P3_SequenceManager> ().AnimationDone ();
+	}
+
+}
---
+++ Assets/Scripts/P3_SubHolder.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P3_SubHolder : MonoBehaviour {
+
+	public bool allowClick = false;	// true if trigger allowed
+	public float speed = 1;				// animation speed
+	public GameObject subtitle;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.touchCount > 0) && (Input.GetTouch (0).phase == TouchPhase.Began)) {
+
+			if (allowClick) {
+				Ray raycast = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+				RaycastHit raycastHit;
+				if (Physics.Raycast (raycast, out raycastHit)) {
+					if (raycastHit.collider.name == gameObject.name) {
+						allowClick = false;
+						subtitle.GetComponent<P3_Subtitles> ().FadeOut ();
+					}
+
+				}
+			} else {
+				subtitle.GetComponent<P15_Subtitle> ().setToEnd ();
+			}
+		} else if (Input.GetMouseButtonDown (0)) {
+			if (allowClick) {
+				Ray raycast = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit raycastHit;
+				if (Physics.Raycast (raycast, out raycastHit)) {
+					if (raycastHit.collider.name == gameObject.name) {
+						allowClick = false;
+						subtitle.GetComponent<P3_Subtitles> ().FadeOut ();
+
+					}
+
+				}
+			} else {
+				subtitle.GetComponent<P3_Subtitles> ().setToEnd ();
+			}
+		}
+
+	}
+}
