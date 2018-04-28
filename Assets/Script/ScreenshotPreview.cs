619bad841c507c7e813082c925163d48dcf5fccf
Kevin <kevin18.liem@gmail.com>
2018-04-28 15:03:01 +0700

    Add Page 13 assets

--- Assets/Script/ScreenshotPreview.cs
+++ Assets/Script/ScreenshotPreview.cs
@@ -20,7 +20,7 @@ public class ScreenshotPreview : MonoBehaviour {
 		string pathToFile = files [0];
 		Texture2D texture = GetScreenshotImage (pathToFile);
 		Sprite sp = Sprite.Create (texture, new Rect (0, 0, texture.width, texture.height), new Vector2 (0.5f, 0.5f));
-		canvas.GetComponent<Image> ().sprite = sp;
+		canvas.GetComponent<SpriteRenderer> ().sprite = sp;
 	}
 
 	Texture2D GetScreenshotImage(string filePath) {
