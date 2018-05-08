226fdbb640dc27047518adf24ad31fca010cb251
clement.andreas97 <clement.andreas97@gmail.com>
2018-05-08 21:19:16 +0700

    Edit Color and Shading Navigation Panel; Added Change Page Script to each item; Added Conditional Page in Navigation

--- Assets/Scripts/ToggleNavigation.cs
+++ Assets/Scripts/ToggleNavigation.cs
@@ -7,15 +7,38 @@ public class ToggleNavigation : MonoBehaviour {
 	public GameObject NavigationPanel;
 	public GameObject SliderPanel;
 	public GameObject Grey;
+	public GameObject Telepon;
+	public GameObject VideoCall;
+	public bool unlockTelepon = false;
+	public bool unlockVideoCall = false;
+	[SerializeField]
 	public void toggleAnimation() {
 		if (NavigationPanel.activeInHierarchy) {
 			NavigationPanel.SetActive (false);
 			SliderPanel.SetActive (false);
 			Grey.SetActive (false);
+			if (unlockTelepon) {
+				Telepon.SetActive (false);
+			}
+			if (unlockVideoCall) {
+				VideoCall.SetActive (false);
+			}
+			Time.timeScale = 1;
 		} else {
 			NavigationPanel.SetActive (true);
 			SliderPanel.SetActive (true);
 			Grey.SetActive (true);
+			if (unlockTelepon) {
+				Telepon.SetActive (true);
+			} else {
+				Telepon.SetActive (false);
+			}
+			if (unlockVideoCall) {
+				VideoCall.SetActive (true);
+			} else {
+				VideoCall.SetActive (false);
+			}
+			Time.timeScale = 0;
 		}
 	}
 }
