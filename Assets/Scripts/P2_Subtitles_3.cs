61391fb80c1ec1a719fd2cddbb324ceb9adcc267
Kevin <kevin18.liem@gmail.com>
2018-05-08 15:37:15 +0700

    Add Home button

--- Assets/Scripts/P2_Subtitles_3.cs
+++ Assets/Scripts/P2_Subtitles_3.cs
@@ -66,7 +66,10 @@ public class P2_Subtitles_3 : MonoBehaviour {
 			yield return new WaitForSeconds (sec);
 
 			// Highlight
-			HighlightText();
+			if (PlayerPrefs.GetString ("HighlightText") == "on") {
+				HighlightText ();
+				Debug.Log ("On highlight");
+			}
 
 			// increment
 			idx++;
---
+++ Assets/homePopup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homePopup : MonoBehaviour {
+
+	public GameObject homesPopup;
+	[SerializeField]
+	public void HomesPopupOn() {
+		homesPopup.SetActive (true);
+		Time.timeScale = 0;
+	}
+}
