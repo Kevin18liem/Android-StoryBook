e1b72fbb1c3f525406f09e3caa836a8fc7b22c06
Edwin Tandiono <tandiono98@gmail.com>
2018-05-12 18:15:03 +0700

    Added playerpref to save name on buku milik

---
+++ Assets/Scripts/BukuMilik_nextButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BukuMilik_nextButton : MonoBehaviour {
+
+	public InputField inputfield;
+	public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ToNextPage() {
+		PlayerPrefs.SetString ("PlayerName", inputfield.text);
+		SceneManager.LoadScene (nextScene);
+	}
+
+}
