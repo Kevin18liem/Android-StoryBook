096950c6e9873726d696ec65c7bd65f3b53053f5
Edwin Tandiono <tandiono98@gmail.com>
2018-05-12 14:27:08 +0700

    Revised page 9B

--- Assets/Scripts/P9B_SequenceManager.cs
+++ Assets/Scripts/P9B_SequenceManager.cs
@@ -41,7 +41,8 @@ public class P9B_SequenceManager : MonoBehaviour {
 			case 0:
 				{
 					Debug.Log ("seq 0 : delaying");
-					StartCoroutine (Delay (4));
+					spriteAnak.GetComponent<Animator> ().SetFloat ("mood", 0);
+					StartCoroutine (Delay (2));
 					sequence++;
 					inSequence = false;
 					break;
@@ -50,7 +51,6 @@ public class P9B_SequenceManager : MonoBehaviour {
 				{
 					Debug.Log ("seq 1 : enabling ayah");
 					bgAyah.SetActive (true);
-					balonAyahKecil.SetActive (true);
 					spriteAyah.SetActive (true);
 					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
 					balonAyahKecil.GetComponent<Animator> ().SetTrigger ("in");
@@ -61,93 +61,41 @@ public class P9B_SequenceManager : MonoBehaviour {
 			case 2:
 				{
 					Debug.Log ("seq 2 : anak reply");
-					balonAyahKecil.SetActive (false);
-					balonAnak.SetActive (true);
 					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
 					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
 					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (0);
 					sequence++;
 					break;
 				}
-			case 3: 
-				{
-					Debug.Log ("seq 3 : anak reply 2");
-					spriteAnak.GetComponent<Animator> ().SetFloat ("mood", 0);
-					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
-					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
-					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (1);
-					sequence++;
-					break;
-				}
-			case 4:
+			case 3:
 				{
 					Debug.Log ("seq 4 : ayah reply");
-					balonAnak.SetActive (false);
-					balonAyahKecil.SetActive (true);
 					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
 					balonAyahKecil.GetComponent<Animator> ().SetTrigger ("in");
 					subtitleAyahKecil.GetComponent<P9B_Subtitles> ().DoSub (1);
 					sequence++;
 					break;
 				}
+			case 4: 
+				{
+					Debug.Log ("seq 3 : anak reply 2");
+					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
+					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
+					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (1);
+					sequence++;
+					break;
+				}
 			case 5:
 				{
 					Debug.Log ("seq 5 : ayah reply 2");
-					balonAyahKecil.SetActive (false);
-					balonAyahBesar.SetActive (true);
-					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
-					balonAyahBesar.GetComponent<Animator> ().SetTrigger ("in");
-					subtitleAyahBesar.GetComponent<P9B_Subtitles> ().DoSub (0);
-					sequence++;
-					break;
-				}
-			case 6:
-				{
-					Debug.Log ("seq 6 : ayah reply 3");
-					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
-					balonAyahBesar.GetComponent<Animator> ().SetTrigger ("in");
-					subtitleAyahBesar.GetComponent<P9B_Subtitles> ().DoSub (1);
-					sequence++;
-					break;
-				}
-			case 7:
-				{
-					Debug.Log ("seq 7 : anak reply 3");
-					balonAyahBesar.SetActive (false);
-					balonAnak.SetActive (true);
-					spriteAnak.GetComponent<Animator> ().SetFloat ("mood", 1);
-					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
-					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
-					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (2);
-					sequence++;
-					break;
-				}
-			case 8:
-				{
-					Debug.Log ("seq 8 : ayah reply 4");
-					balonAnak.SetActive (false);
-					balonAyahKecil.SetActive (true);
 					spriteAyah.GetComponent<Animator> ().SetTrigger ("ngomong");
 					balonAyahKecil.GetComponent<Animator> ().SetTrigger ("in");
 					subtitleAyahKecil.GetComponent<P9B_Subtitles> ().DoSub (2);
 					sequence++;
 					break;
 				}
-			case 9:
+			case 6:
 				{
-					Debug.Log ("seq 9 : anak reply 4");
-					balonAyahKecil.SetActive (false);
-					balonAnak.SetActive (true);
-					spriteAnak.GetComponent<Animator> ().SetFloat ("mood", 1);
-					spriteAnak.GetComponent<Animator> ().SetTrigger ("ngomong");
-					balonAnak.GetComponent<Animator> ().SetTrigger ("in");
-					subtitleAnak.GetComponent<P9B_Subtitles> ().DoSub (3);
-					sequence++;
-					break;
-				}
-			case 10:
-				{
-					balonAnak.SetActive (false);
 					nextPageButton.SetActive (true);
 					break;
 				}
