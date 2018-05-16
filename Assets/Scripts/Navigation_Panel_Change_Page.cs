4bc1e14e59f7342b5a7c7b917c982f3ce2ad0bda
Kevin <kevin18.liem@gmail.com>
2018-05-16 22:53:20 +0700

    Add SFX effect

--- Assets/Scripts/Navigation_Panel_Change_Page.cs
+++ Assets/Scripts/Navigation_Panel_Change_Page.cs
@@ -8,5 +8,8 @@ public class Navigation_Panel_Change_Page : MonoBehaviour {
 	public void ToNextPage() {
 		SceneManager.LoadScene (nextScene);
 		Time.timeScale = 1;
+		if (PlayerPrefs.GetString ("Musik") == "on") {
+			GetComponent<AudioSource> ().Play ();
+		}
 	}
 }
