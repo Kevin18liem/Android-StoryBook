615ca1021f10855c13d4afc3915b94ed41cc4c8f
Edwin Tandiono <tandiono98@gmail.com>
2018-05-16 20:48:41 +0700

    Added fade on scroll on untukortu

---
+++ Assets/Scripts/UntukOrtuScroll.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UntukOrtuScroll : MonoBehaviour {
+
+	public GameObject topFade;
+	public GameObject bottomFade;
+
+	private Scrollbar scroll;
+
+	// Use this for initialization
+	void Start () {
+
+		topFade.SetActive (false);
+		bottomFade.SetActive (false);
+		scroll = GetComponent<Scrollbar>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (scroll.value == 1) {
+			topFade.SetActive (false);
+		} else {
+			if (!topFade.activeSelf) {
+				topFade.SetActive (true);
+			}
+		}
+
+		if (scroll.value == 0) {
+			bottomFade.SetActive (false);
+		} else {
+			if (!bottomFade.activeSelf) {
+				bottomFade.SetActive (true);
+			}
+		}
+
+	}
+
+
+
+}
