a2f4293a604b3fe1cb5017dc9355c2c6cdb52571
Kevin <kevin18.liem@gmail.com>
2018-05-17 11:29:00 +0700

    fix main menu scene bug

---
+++ Assets/ConfigMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeScale == 0)
+			Time.timeScale = 1;
+	}
+}
