c7b57c87a71bb970f70b961bc443367804767877
clement.andreas97 <clement.andreas97@gmail.com>
2018-05-26 09:37:28 +0700

    Page 5 Ibu Changed, IDLE STAY; Page 5 Sound In Progress

---
+++ Assets/Scripts/P5_DelayedAnimationSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P5_DelayedAnimationSound : MonoBehaviour {
+	private AudioSource audioSource;
+
+	void Start() {
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.PlayDelayed (2);
+	}
+
+	void Update() {
+	}
+}
