b83bce84849ec11fe5dc2ce6a76bb7c6a9476ed4
Edwin Tandiono <tandiono98@gmail.com>
2018-05-27 15:29:17 +0800

    Fixed page 5 navigation

--- Assets/Scripts/NavButton.cs
+++ Assets/Scripts/NavButton.cs
@@ -8,6 +8,8 @@ public class NavButton : MonoBehaviour {
 	public bool toRight;
 	public ScrollRect rect;
 	public float slideVal = 0.1f;
+	public GameObject fadeKanan;
+	public GameObject fadeKiri;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +18,19 @@ public class NavButton : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (rect.horizontalNormalizedPosition <= 0.05f) {
+			fadeKiri.SetActive (false);
+		} else {
+			fadeKiri.SetActive (true);
+		}
+
+		if (rect.horizontalNormalizedPosition >= 0.95f) {
+			fadeKanan.SetActive (false);
+		} else {
+			fadeKanan.SetActive (true);
+		}
+
 	}
 
 	public void Scroll() {
