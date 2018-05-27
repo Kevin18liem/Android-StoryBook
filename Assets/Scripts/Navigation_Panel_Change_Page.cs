47358d98e19a58ed0ee6a190f62120d3db5ed176
clement.andreas97 <clement.andreas97@gmail.com>
2018-05-27 16:19:17 +0700

    Page 8, 9a, 9b, 10, 11, 12, 13, 14, 15 Added New Navigation Panel, Fix Navigation Access from Setting and Home Button; Fix Subtitle Play After navigating from certain page

--- Assets/Scripts/Navigation_Panel_Change_Page.cs
+++ Assets/Scripts/Navigation_Panel_Change_Page.cs
@@ -8,6 +8,7 @@ public class Navigation_Panel_Change_Page : MonoBehaviour {
 	public void ToNextPage() {
 		SceneManager.LoadScene (nextScene);
 		Time.timeScale = 1;
+		AudioListener.pause = false;
 //		if (PlayerPrefs.GetString ("Musik") == "on") {
 //			GetComponent<AudioSource> ().Play ();
 //			GameObject.FindWithTag("subtitle").GetComponent<AudioSource>().UnPause();
